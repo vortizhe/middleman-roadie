42d05a7627c01b4425a4ce74527500785d00210d
Victor Ortiz <kespers@gmail.com>
2016-03-20 22:39:42 +0100

    Add middleware to pass html response through roadie

--- lib/middleman-roadie/extension.rb
+++ lib/middleman-roadie/extension.rb
@@ -15,12 +15,47 @@ module Middleman
     end
 
     def after_configuration
-      # Do something
+      app.use Rack, options
     end
 
     # A Sitemap Manipulator
     # def manipulate_resource_list(resources)
     # end
 
+    class Rack
+      def initialize(app, options = {})
+        @app = app
+        @options = options
+      end
+
+      def call(env)
+        status, headers, body = @app.call(env)
+
+        if headers.key? 'Content-Type' and headers['Content-Type'] =~ /html/
+          content = ''
+
+          body.each do |part|
+            content << part
+          end
+
+          email = transform_html(content)
+
+          headers['Content-Length'] = email.bytesize.to_s if headers['Content-Length']
+          [status, headers, [email]]
+        else
+          [status, headers, body]
+        end
+      ensure
+        body.close if body.respond_to?(:close)
+      end
+
+      private
+
+      def transform_html(old_html)
+        document = ::Roadie::Document.new old_html
+        @options.apply_to document
+        document.transform
+      end
+    end
   end
 end
